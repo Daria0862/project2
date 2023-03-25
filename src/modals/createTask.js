025fc6969955b5fb1af636daae928a47a1fbf73e
Tehmeena Chattha <119902254+Daria0862@users.noreply.github.com>
2023-03-25 16:01:41 +0000

    added modal form

--- src/modals/createTask.js
+++ src/modals/createTask.js
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, MoModalFooter } from 'reactstrap';
 
-const createTask = ({modal, toggle}) => {
+const CreateTaskPopup = ({ modal, toggle }) => {
+    const [taskName, setTastName] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+
+        if(name === "taskName"){
+            setTastName(value)
+        }else{
+            setDescription(value)
+        }
+    }
+
     return (
         <div>
             <Modal isOpen={modal} toggle={toggle}>
-                <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+                <ModalHeader toggle={toggle}>Create Task</ModalHeader>
                 <ModalBody>
-                     <form>
-                        <div className = "form-group">
+                    <form>
+                        <div className="form-group">
                             <label>Task Name</label>
-                            <input type="text" className = "form-control" />
-
+                            <input type="text" className="form-control" value={taskName} onChange = {handleChange} name = "taskName"/>
                         </div>
-                        <div className = "form-group">
+                        <div className="form-group">
                             <label>Description</label>
-                            <textarea rows = "5" className = "form-control"></textarea>
+                            <textarea rows="5" className="form-control" value={description} onChange = {handleChange} name = "description"></textarea>
 
                         </div>
 
-                     </form>
-            </ModalBody>
-            <ModalFooter>
-                <Button color= "primary" onClick={toggle}>Create</Button>{''}
-            </ModalFooter>
+                    </form>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={toggle}>Create</Button>{''}
+                </ModalFooter>
             </Modal>
-            </div>
+        </div>
     )
 }
