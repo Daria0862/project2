5796f6174d4ccd0d5e2b18aa1fe43b5fb9fe7ab3
Tehmeena Chattha <119902254+Daria0862@users.noreply.github.com>
2023-03-25 14:59:15 +0000

    centered header

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 const TodoList = () => {
     return (
-        <div>
+        <>
+        <div className='header text-center'> 
             <h3>MotivateMe</h3>
             <button className='btn btn-primary'>Create Task</button>
         </div>
+        <div className='task-container'>
+
+        </div>
+        </>
     );
 };
 
-export default TodoList;''
+export default TodoList;
