26bb28af770ad78bc4be7aa8624e9a76527c7d65
Sumayyah Mosafeer <119902274+Tina4126@users.noreply.github.com>
2023-03-26 18:51:54 +0100

    tasklist

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -52,7 +52,7 @@ const TodoList = () => {
             <button className='btn btn-primary mt-2' onClick={() => setModal(true)}>Create Task</button>
         </div>
         <div className='task-container'>
-            {taskList.map((obj, index) => <Card taskObj = {obj} index = {index} deleteTask = {deleteTask}/>)}
+            {taskList.map((obj, index) => <Card taskObj = {obj} index = {index} deleteTask = {deleteTask} updateListArray = {updateListArray}/>)}
 
         </div>
         <CreateTask toggle = {toggle} modal = {modal} save = {saveTask}/>
